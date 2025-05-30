287e152bb5dcaa0673c57411f22e26fafc219099
angelo-lepore <angelo.lepore94@gmail.com>
2025-05-30 11:04:00 +0200

    consegna-esercizio

---
+++ main.js
@@ -0,0 +1,14 @@
+/*L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+const primo_numero = Number(prompt("Inserire il primo numero:"));
+
+const secondo_numero = Number(prompt("Inserire il secondo numero:"));
+
+if (primo_numero > secondo_numero) {
+    console.log("Il numero maggiore è:", primo_numero);
+} else if (secondo_numero > primo_numero) {
+    console.log("Il numero maggiore è:", secondo_numero);
+} else {
+    console.log("I due numeri sono uguali:", primo_numero);
+}
